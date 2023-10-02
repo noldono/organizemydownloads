7948d437ca4622cdafd1351043abc69b089c5645
Adam Lahouar <adamlahouar@vt.edu>
2023-10-02 12:43:01 -0400

    Created method that identifies duplicate files from a list, updated file class to get file name without type

--- desktop/organize.py
+++ desktop/organize.py
@@ -22,3 +22,37 @@ def organize(files):
 
         if not organized:
             os.renames(file.path, os.path.join('Other', file.name))
+
+
+def get_duplicates(files):
+    # keep track of duplicate files and unique files
+    duplicates = []
+    unique_files = []
+
+    for file in files:
+        duplicate = False
+        for unique_file in unique_files:
+            # if file types differ, skip
+            if file.type != unique_file.type:
+                continue
+
+            # if file sizes differ, skip
+            if file.size != unique_file.size:
+                continue
+
+            # if file names do not follow the Windows duplicate file name convention, skip
+            # example: 'file.txt' and 'file (1).txt.'
+            if file.name not in unique_file.name and unique_file.name not in file.name:
+                continue
+
+            # if file types, sizes, and names match, add to duplicates
+            duplicates.append(file)
+            duplicate = True
+            break
+
+        # if file is not a duplicate, add to unique files
+        if not duplicate:
+            unique_files.append(file)
+
+    # return the list of duplicate files
+    return duplicates
