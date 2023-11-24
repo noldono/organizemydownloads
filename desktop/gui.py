3c95350077626fde1b83031ab34f3872ae3d48c2
Adam Lahouar <adamlahouar@vt.edu>
2023-11-24 03:00:15 -0500

    Added file identification by date and size with user-selectable thresholds, added warning messages before deleting or recycling files

--- desktop/gui.py
+++ desktop/gui.py
@@ -1,13 +1,29 @@
-from datetime import date
+from datetime import date, timedelta
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QDesktopWidget, QMenuBar, QPushButton, QLineEdit, \
-    QHBoxLayout, QLabel, QComboBox, QMessageBox, QTableWidgetItem, QFileDialog
+    QHBoxLayout, QLabel, QComboBox, QMessageBox, QTableWidgetItem, QFileDialog, QActionGroup, QMenu, QAction
 from PyQt5.QtGui import QIcon
 
 import functions
-import organize
+# import organize
 from filetable import FileTable
+from file import File
+
+DATE_THRESHOLDS: dict[str, timedelta] = {
+    "1 Month": timedelta(days=30),
+    "3 Months": timedelta(days=30 * 3),
+    "6 Months": timedelta(days=30 * 6),
+    "1 Year": timedelta(days=365),
+    "2 Years": timedelta(days=365 * 2),
+    "5 Years": timedelta(days=365 * 5),
+}
+
+SIZE_THRESHOLDS: dict[str, int] = {
+    "100 MB": 100 * 1024 ** 2,
+    "500 MB": 500 * 1024 ** 2,
+    "1 GB": 1024 ** 3
+}
 
 
 class MainWindow(QMainWindow):
@@ -20,6 +36,10 @@ class MainWindow(QMainWindow):
 
         # initialize layout
         self.central_layout = QVBoxLayout(self)
+
+        self.selected_date_threshold: timedelta = DATE_THRESHOLDS["1 Year"]
+
+        self.selected_size_threshold = SIZE_THRESHOLDS["500 MB"]
 
         # initialize menu bar
         self._init_menu_bar()
@@ -66,8 +86,37 @@ class MainWindow(QMainWindow):
 
         # add "Identify" menu with actions
         identify_menu = self.menu_bar.addMenu("Identify")
+        identify_menu.addAction("Identify All").triggered.connect(self._identify_all)
         identify_menu.addAction("Identify Duplicate Files").triggered.connect(self._identify_duplicates)
         identify_menu.addAction("Identify Installers").triggered.connect(self._identify_installers)
+        identify_menu.addAction("Identify Old Files").triggered.connect(self._identify_old_files)
+
+        date_threshold_menu = QMenu("File Date Threshold", self)
+
+        date_threshold_group = QActionGroup(self)
+        for threshold in DATE_THRESHOLDS.keys():
+            threshold_action = QAction(threshold, self)
+            threshold_action.setCheckable(True)
+            if threshold == "1 Year":
+                threshold_action.setChecked(True)
+            threshold_action.triggered.connect(self._handle_date_threshold_change)
+            date_threshold_group.addAction(threshold_action)
+        date_threshold_menu.addActions(date_threshold_group.actions())
+        identify_menu.addMenu(date_threshold_menu)
+
+        identify_menu.addAction("Identify Large Files").triggered.connect(self._identify_large_files)
+
+        size_threshold_menu = QMenu("File Size Threshold", self)
+        size_threshold_group = QActionGroup(self)
+        for threshold in SIZE_THRESHOLDS.keys():
+            threshold_action = QAction(threshold, self)
+            threshold_action.setCheckable(True)
+            if threshold == "500 MB":
+                threshold_action.setChecked(True)
+            threshold_action.triggered.connect(self._handle_size_threshold_change)
+            size_threshold_group.addAction(threshold_action)
+        size_threshold_menu.addActions(size_threshold_group.actions())
+        identify_menu.addMenu(size_threshold_menu)
 
         # add "Delete" menu with actions
         delete_menu = self.menu_bar.addMenu("Delete")
@@ -90,11 +139,17 @@ class MainWindow(QMainWindow):
 
         self.central_layout.addWidget(self.menu_bar)
 
+    def _handle_date_threshold_change(self):
+        self.selected_date_threshold = DATE_THRESHOLDS[self.sender().text()]
+
+    def _handle_size_threshold_change(self):
+        self.selected_size_threshold = SIZE_THRESHOLDS[self.sender().text()]
+
     def _organize(self):
         warning_str = '''
         Please read before proceeding!
         
-        The following function will move all of the files inside your downloads folder into different subdirectories within the downloads folder. We HIGHLY recommend you backup your downloads folder prior to this procedure. You can do this by going to Backup > Backup Downloads Folder. 
+        The following function will move all of the files inside your downloads folder into different subdirectories within the downloads folder. We HIGHLY recommend you backup your downloads folder prior to this procedure. You can do this by going to Backup > Backup Downloads Folder.
         
         Are you sure you wish to proceed?
         '''
@@ -105,16 +160,29 @@ class MainWindow(QMainWindow):
         ret = self.msg.exec_()
 
         if ret == QMessageBox.Yes:
-            organize.organize_into_folders(self.files.values())
+            functions.organize_into_folders(list(self.files.values()))
 
+    def _identify_all(self):
+        self._identify_installers()
+        self._identify_duplicates()
+        self._identify_old_files()
+        self._identify_large_files()
 
     def _identify_installers(self):
         installers = functions.identify_installers(list(self.files.values()))
         self.table.select_files(installers)
 
     def _identify_duplicates(self):
-        duplicate_files = organize.get_duplicates(self.files.values())
+        duplicate_files = functions.get_duplicates(self.files.values())
         self.table.select_files(duplicate_files)
+
+    def _identify_old_files(self):
+        old_files = functions.identify_old_files(list(self.files.values()), self.selected_date_threshold)
+        self.table.select_files(old_files)
+
+    def _identify_large_files(self):
+        large_files = functions.identify_large_files(list(self.files.values()), self.selected_size_threshold)
+        self.table.select_files(large_files)
 
     def _init_copy_button(self):
         # Create search button
@@ -163,10 +231,32 @@ class MainWindow(QMainWindow):
         self.table.deselect_all()
 
     def _delete_selected(self):
-        functions.delete_files(self.table.get_selected_files())
+        files_to_delete = self.table.get_selected_files()
+        if self._show_warning_popup(files_to_delete, True):
+            functions.delete_files(files_to_delete)
 
     def _recycle_selected(self):
-        functions.recycle_files(self.table.get_selected_files())
+        files_to_recycle = self.table.get_selected_files()
+        if self._show_warning_popup(files_to_recycle, False):
+            functions.recycle_files(files_to_recycle)
+
+    def _show_warning_popup(self, file_list: list[File], delete: bool):
+        warning_popup = QMessageBox()
+        warning_popup.setWindowTitle("WARNING")
+        warning_popup.setIcon(QMessageBox.Warning)
+        if len(file_list) == 0:
+            warning_popup.setText("No files selected.")
+            warning_popup.exec_()
+            return False
+
+        keyword = "delete" if delete else "recycle"
+        warning_text = f"Are you sure you want to {keyword} the selected files?"
+        for file in file_list:
+            warning_text += f"\n- {file.name}.{file.type}"
+
+        warning_popup.setText(warning_text)
+        warning_popup.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        return warning_popup.exec_() == QMessageBox.Yes
 
     def _archive_all(self):
         self.popup = QMessageBox()
@@ -201,23 +291,18 @@ class MainWindow(QMainWindow):
             file_object = file_item.data(Qt.UserRole)
 
             # Update the file object based on the checkbox state
-            if item.checkState() == Qt.Checked:
-                file_object.checked = True
-            else:
-                file_object.checked = False
+            file_object.checked = (item.checkState() == Qt.Checked)
 
-    def _update_extensions(self):
-        # return list of all extension
+    def _update_extensions(self) -> None:
+        extensions: list[str] = list()
 
-        ext = []
-
-        for file in self.currentFiles.values():
-            if file.type not in ext:
-                ext.append(file.type)
+        for file in self.files.values():
+            if file.type not in extensions:
+                extensions.append(file.type)
 
         self.comboBox.clear()
         self.comboBox.addItem("All")
-        self.comboBox.addItems(ext)
+        self.comboBox.addItems(extensions)
 
     def _extension_search(self, search_string: str):
         if search_string == 'All':
@@ -228,12 +313,19 @@ class MainWindow(QMainWindow):
 
     def _filename_search(self):
         search_string = self.search_bar.text()
-        if not search_string:
-            self.table.update_table_contents(list(self.files.values()))
+        if search_string:
+            files_to_display = self._search_by_filename(self.table.get_displayed_files(), search_string)
+            self.table.update_table_contents(files_to_display)
         else:
-            # TODO: currently search_by_filename() is case-sensitive, we have to decide if we want to change that or not
-            files_to_display = functions.search_by_filename(self.files, search_string)
-            self.table.update_table_contents(list(files_to_display.values()))
+            self.table.update_table_contents(list(self.files.values()))
+
+    def _search_by_filename(self, files: list[File], search_string: str) -> list[File]:
+        matches: list[File] = list()
+        for file in files:
+            if search_string.lower() in file.name.lower():
+                matches.append(file)
+
+        return matches
 
     # To decide: cut vs. copy
     def _copy_to_folder(self):
